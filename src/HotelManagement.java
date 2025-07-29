4216e3bf357d7904943cbd3ba9ee9573839bb680
thisispraphullverma@gmail.com <thisispraphullverma@gmail.com>
2025-07-30 02:46:09 +0530

    Update file

--- src/HotelManagement.java
+++ src/HotelManagement.java
@@ -1,141 +1,183 @@
 import java.util.Scanner;
 
+public class HotelManagement {
+    static final int ROOMS = 5;
+    static final int FLOOR = 5;
 
-public class HotelManagement{
-    //room booking method--
 
+    //Creating rooms
+    static boolean[][] rooms = new boolean[FLOOR][ROOMS];
 
-     public static void roomBook(int[][] rooms,int floor, int room){
-            if (rooms[floor][room] == 1) {
-        System.out.println("\nRoom is already booked on floor " + floor + ", room number " + room + ".");
-    }     
-    
-        else {
-           rooms[floor][room] = 1; 
-           System.out.println("\nRoom booked successfully on floor " + floor + ", room number " + room + ".");
-    }
-    
+    //Checking availability
+    static void checkAvailability() {
+        System.out.println("A-Available rooms\nX-Unavailable rooms");
+        for (int floor = 0; floor < FLOOR; floor++) {
+            for (int room = 0; room < ROOMS; room++) {
+                if (rooms[floor][room]) {
+                    System.out.print("X ");
+                } else {
+                    System.out.print("A ");
+                }
+            }
+            System.out.println();
         }
-       
-    //Check availability
-    public static void availability(int[][] rooms, int floor, int room){
-        if(rooms[floor][room]==0){
-            System.out.println("\nRoom available.");
-        }
-        else{
-            System.out.println("\nRoom not available.");
-        }
+
     }
 
+    //Booking a room
+    static void BookRoom(Scanner sc) {
 
-    //Checkout Method
-    public static int checkout(int[][] rooms, int floor, int room){
-        if(rooms[floor][room]==1){
-            System.out.println("\nCheckout successful. ");
-            rooms[floor][room]=0;
+        System.out.println("Enter Floor number: ");
+        int floor = sc.nextInt();
+
+        System.out.println("Enter Room number: ");
+        int room = sc.nextInt();
+        if (floor > FLOOR - 1 || room > ROOMS - 1) {
+            System.out.println("Please enter valid numbers");
+        } else if (!rooms[floor][room]) {
+            rooms[floor][room] = true;
+            System.out.println("Room booked on the floor " + floor + " and the room number is " + room);
+        } else {
+            System.out.println("Room isn't available");
         }
-        else{
-            System.out.println("\nRoom wasn't occupied. ");
-            return 0;
-        }
-        return 1;
+        sc.nextLine();
+
     }
+    //Implementing checkout function--
+
+    static void Checkout(Scanner sc) {
+        System.out.println("Enter Floor number: ");
+        int floor = sc.nextInt();
+
+        System.out.println("Enter Room number: ");
+        int room = sc.nextInt();
+
+        if (floor > FLOOR - 1 || room > ROOMS - 1) {
+            System.out.println("Please enter valid numbers");
+        } else if (rooms[floor][room]) {
+            rooms[floor][room] = false;
+            System.out.println("Checkout Successful");
+        } else {
+            System.out.print("Room wasn't booked on the floor " + floor + " with room number " + room);
+
+        }
+        sc.nextLine();
+
+    }
+
+    //Check-in method
+    static void Checkin(Scanner sc) {
+        System.out.print("Enter Your Name: ");
+        String name = sc.nextLine();
+
+        System.out.print("Enter Your ID number: ");
+        String ID = sc.nextLine();
+
+        System.out.println("Enter Floor number: ");
+        int floor = sc.nextInt();
+        sc.nextLine();
+
+
+        System.out.println("Enter Room number: ");
+        int room = sc.nextInt();
+        sc.nextLine();
+
+        if (floor > FLOOR - 1 || room > ROOMS - 1) {
+            System.out.println("Please enter valid numbers");
+        } else if (rooms[floor][room]) {
+            System.out.println("Welcome " + name + ", check-in successful at Floor " + floor + ", Room " + room);
+        } else {
+            System.out.println("Room not booked yet. Please book a room first.");
+        }
+
+
+    }
+
+    static void ShowMenu() {
+        System.out.println("\n==== Hotel booking Menu==== \n");
+        System.out.println("1. Check availability");
+        System.out.println("2. Book Room");
+
+        System.out.println("3. Checkin Room");
+        System.out.println("4. Checkout Room");
+
+        System.out.println("5. Payments");
+        System.out.println("6. Exit\n");
+        System.out.print("Enter Choice: ");
+    }
+
+
     public static void main(String[] args) {
-        System.out.println("\nWelcome to the Hotel Management System\n\nWe serve our customer using this simple app\n\nWe have 3 Floors with floor number[0-1-2] and a total of 12 rooms with room number [0-1-2-3]\nThanks for using our services.\n");
-
-
         Scanner sc = new Scanner(System.in);
 
+        int option;
 
-        //2-D array for floor and rooms
-        //Creating an initially empty array using a for loop.
+        do {
+            ShowMenu();
+            option = sc.nextInt();
+            sc.nextLine();
 
+            switch (option) {
+                case 1:
+                    checkAvailability();
+                    break;
 
-        int[][] rooms = new int[3][4];
-        for (int i = 0; i < rooms.length; i++) {
-            for (int j = 0; j < rooms[i].length; j++) {
-                rooms[i][j] = 0;
-            }
-        }
-        int option = 0;
-       
-        do{
-            //Asking inputs from user for room-booking
-        System.out.println("\nChoose an option:\n1. Room Booking\n2. Check availability\n3. Checkout\n4. Payments\n5. Quit\n");
-       
-        option = sc.nextInt();
-       
-       
-        switch (option) {
-            case 1:
-                  System.out.print("Please Enter the floor number for room Booking [0-1-2]: ");
-                  int floor = sc.nextInt();
-                  System.out.print("Please Enter the room number for room Booking [0-1-2-3]: ");
-                  int roomNumber = sc.nextInt();
-                  roomBook(rooms, floor, roomNumber);
-                break;
-               
-            case 2:
-                  System.out.print("Please Enter the floor number for checking the availability: ");
-                  floor = sc.nextInt();
-                  System.out.print("Please Enter the room number for checking the availability: ");
-                  roomNumber = sc.nextInt();
-                  availability(rooms, floor, roomNumber);
-                break;
-            case 3:
-                  System.out.println("Please Enter the floor number for checkout: ");
-                  floor = sc.nextInt();
-                  System.out.println("Please Enter the room number for checkout: ");
-                  roomNumber = sc.nextInt();
-                  checkout(rooms, floor, roomNumber);
-                  break;
+                case 2:
+                    BookRoom(sc);
+                    break;
 
+                case 3:
+                    Checkin(sc);
+                    break;
 
-            case 4:
-                  System.out.println("Please Choose the payment method:\n1. UPI\n2. Cash\n3. Debit Card\n");
-                  int payment = sc.nextInt();
-                  switch (payment) {
-                    case 1:
-                       
-                        System.out.println("ROOM RENT - 8000$\nGST + UPI CHARGES - 352$\nYour Total bill = " + (8000+352));
-                        System.out.println("Thanks for staying...");
+                case 4:
+                    Checkout(sc);
+                    break;
 
-
-                        option = 5;
-                        break;
-
-
-                    case 2:
-                        System.out.println("ROOM RENT - 8000$\nGST - 122$\nYour Total bill = " + (8000+122));
-                        System.out.println("Thanks for staying...");
-                        option = 5;
-                        break;
-
-
-                    case 3:
-                        System.out.println("ROOM RENT - 8000$\nGST + Card Charges - 252$\nYour Total bill = " + (8000+252));
-                        System.out.println("Thanks for staying...");
-                        option = 5;
-
-
-
-
-                        break;
-
-
-                 
-                    default:
-                        break;
-                  }
-                  break;
                 case 5:
-                  System.out.println("Sorry to see you go...");
-       
-            default:
-                break;
-        }
-        }while(option!=5);
-       
+                    System.out.println("Please Choose the payment method:\n1. UPI\n2. Cash\n3. Debit Card\n");
+                    int payment = sc.nextInt();
+                    sc.nextLine();
+                    switch (payment) {
+                        case 1:
+
+                            System.out.println("ROOM RENT - 8000$\nGST + UPI CHARGES - 124$\nYour Total bill = " + (8000 + 124) + "$");
+                            System.out.println("Thanks for staying...");
 
 
-    }}
+                            break;
+
+                        case 2:
+                            System.out.println("ROOM RENT - 8000$\nGST - 122$\nYour Total bill = " + (8000 + 122) + "$");
+                            System.out.println("Thanks for staying...");
+
+                            break;
+
+
+                        case 3:
+                            System.out.println("ROOM RENT - 8000$\nGST + Card Charges - 252$\nYour Total bill = " + (8000 + 252) + "$");
+                            System.out.println("Thanks for staying...");
+
+
+                            break;
+
+
+                        default:
+                            System.out.println("Invalid choice");
+                            break;
+
+                    }
+                    break;
+
+                case 6:
+                    break;
+                default:
+                    System.out.println("Invalid choice");
+            }
+
+        } while (option != 6);
+        sc.close();
+
+    }
+}
+
